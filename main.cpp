6796452be6a943938de40e9f14ba5f27a397927d
NHypocrite's Ubuntu <zmdhpc@163.com>
2021-11-24 20:02:52 +0800

    Add all my files

---
+++ main.cpp
@@ -0,0 +1,162 @@
+#include "svpng.inc"
+#include <iostream>
+
+#include "camera.h"
+#include "hitable_list.h"
+#include "sphere.h"
+#include <float.h>
+#include <omp.h>
+#include <random>
+// #include <ctime>
+
+#define W 800
+#define H 400
+const double infinity = std::numeric_limits<double>::infinity();
+
+// normal shader
+Vec3 color(const Ray &r, hitable *world) {
+  hit_record rec;
+  if (world->hit(r, 0.0, FLT_MAX, rec)) {
+    //有撞点：即为球体，将球体颜色设置为球在该点的单位法向量的色彩表映射值
+    return 0.5 *
+           Vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
+  } else {
+    // 无撞点：将颜色设置为背景色
+    Vec3 unit_direction = unit_vector(r.Direction());
+    float t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * Vec3(1.0, 1.0, 1.0) +
+           t * Vec3(0.5, 0.7, 1.0); // white, light blue
+  }
+}
+
+/******************** material shader ************************/
+/**
+ * @brief 光线追踪函数
+ *
+ * @param r: 光线
+ * @param world： 场景中的物体集合
+ * @param depth: 光线弹射次数，到达最大次数之后，返回0
+ *
+ */
+Vec3 ray_color(const Ray &r, hitable *world, int depth) {
+  hit_record rec;
+  //如果光线到达反射次数,停止
+  if (depth <= 0)
+    return Vec3(0,0,0);
+
+  //如果光线与物体相交,设置反射后的颜色
+  if (world->hit(r, 0.001, infinity, rec)) {
+    Ray scattered;
+    Vec3 attenuation;
+    if (rec.mat->scatter(r, rec, scattered, attenuation))
+      return attenuation * ray_color(scattered, world, depth-1);
+    return Vec3(0,0,0);
+  }
+  //如果光线与任何物体不相交,设置背景颜色
+  Vec3 unit_direction = unit_vector(r.Direction());
+  auto t = 0.5*(unit_direction.y() + 1.0);
+  return (1.0-t)*Vec3(1.0, 1.0, 1.0) + t*Vec3(0.5, 0.7, 1.0);
+}
+
+
+
+
+unsigned char img[W * H * 3];
+
+int main() {
+  unsigned char *p = img;
+
+  // 声明
+  Camera cam;
+
+  int SamplingRate = 100; //采样次数
+  int Depth = 50; //光线弹射次数, 如果是一个空心玻璃球的话,弹射次数至少>4次，思考一下？
+
+  /*将所有能够被撞击的物体信息保存在列表中*/
+  std::vector<hitable *> list;
+/*--------------------------------------------------------------------*/
+  // //散射球体 
+  // //设定材质
+  // // Material *diffuse_ground = new Material(Vec3(0.8, 0.8, 0.0), Diffuse);
+  // // Material *diffuse_center = new Material(Vec3(0.7, 0.3, 0.3), Diffuse);
+  // Material *diffuse_ground = new Material(Vec3(0.5, 0.5, 0.5), Diffuse);
+  // Material *diffuse_center = new Material(Vec3(0.5, 0.5, 0.5), Diffuse);
+  // //物体加入列表
+  // list.push_back(new sphere(Vec3(0, -100.5, -1), 100, diffuse_ground));
+  // list.push_back(new sphere(Vec3(0, 0, -1), 0.5, diffuse_center));
+/*--------------------------------------------------------------------*/
+  // // //金属球体
+  // //设定材质
+  // Material *diffuse_ground = new Material(Vec3(0.8, 0.8, 0.0), Diffuse);
+  // Material *diffuse_center = new Material(Vec3(0.7, 0.3, 0.3), Diffuse);
+  // Material *metal_left = new Material(Vec3(0.8, 0.8, 0.0), 0.3, Metal);
+  // Material *metal_right = new Material(Vec3(0.8, 0.6, 0.2), 1.0, Metal);
+    
+  // //物体加入列表
+  // list.push_back(new sphere(Vec3(0, -100.5, -1), 100, diffuse_ground));
+  // list.push_back(new sphere(Vec3(0, 0, -1), 0.5, diffuse_center));
+  // list.push_back(new sphere(Vec3(-1, 0, -1), 0.5, metal_left));
+  // list.push_back(new sphere(Vec3(1, 0, -1), 0.5, metal_right));
+/*--------------------------------------------------------------------*/
+  //透明球体
+  //设定材质
+  Material *diffuse_ground = new Material(Vec3(0.8, 0.8, 0.0), Diffuse);
+  Material *diffuse_center = new Material(Vec3(0.1, 0.2, 0.5), Diffuse);
+  Material *dielectric_left = new Material(1.5, Dielectric);
+  Material *metal_right = new Material(Vec3(0.8, 0.6, 0.2), 1.0, Metal);
+    
+  //物体加入列表
+  list.push_back(new sphere(Vec3(0, -100.5, -1), 100, diffuse_ground));
+  list.push_back(new sphere(Vec3(0, 0, -1), 0.5, diffuse_center));
+  list.push_back(new sphere(Vec3(-1, 0, -1), 0.5, dielectric_left));
+  list.push_back(new sphere(Vec3(-1, 0, -1), -0.4, dielectric_left));    // 空心玻璃球
+  list.push_back(new sphere(Vec3(1, 0, -1), 0.5, metal_right));
+/*--------------------------------------------------------------------*/
+  //将物体组成的表加入碰撞列表
+  hitable *world = new hitable_list(list, list.size());
+  int tmp = 0;
+  // // Windows随机数
+  // srand((unsigned)time(NULL));
+  #pragma omp parallel for     //并行
+  for (int j = H - 1; j >= 0; j--) {
+    // if(omp_get_thread_num()==1)
+    if(j % 20 == 1)
+    std::cerr << "\rComputing: " << ". .  .   .   ." << std::flush;
+    else if(j % 20 == 11)
+    std::cerr << "\rComputing: " << "              " << std::flush;
+    for (int i = 0; i < W; i++) {
+      int tmp=3*((399-j)*800+i);
+      Vec3 col(0, 0, 0);
+      for (int s = 0; s < SamplingRate; s++) {
+        float u = float(i + drand48()) / float(W);
+        float v = float(j + drand48()) / float(H);
+        // // Windows随机数
+        // float u = float(i + rand()) / float(W);
+        // float v = float(j + rand()) / float(H);
+        Ray r = cam.getRay(u, v);
+        // col += color(r, world);
+        col += ray_color(r, world, Depth);
+      }
+      col /= float(SamplingRate);
+      auto scale = 1.0 / SamplingRate;
+
+// #pragma omp critical
+
+      p[tmp] = int(255.99 * sqrt(col[0]));
+      p[tmp+1] = int(255.99 * sqrt(col[1]));
+      p[tmp+2] = int(255.99 * sqrt(col[2]));
+      
+
+      // std::cerr << "\rPixels remaining in this line: " << W-i <<' '<< std::flush; 
+    }
+  }
+
+  svpng(fopen("bg3_50_2.png", "wb"), W, H, img, 0);
+  std::cout << "finished" << std::endl;
+
+  //释放资源
+  // delete diffuse_center, diffuse_ground;
+  // delete diffuse_center, diffuse_ground, metal_left, metal_right;
+  delete diffuse_center, diffuse_ground, dielectric_left, metal_right;
+  return 0;
+}
---
+++ material.cpp
@@ -0,0 +1,135 @@
+#include "material.h"
+
+float get_random_float(float min, float max) {
+  static std::random_device dev;
+  static std::mt19937 rng(dev());
+  static std::uniform_real_distribution<float> dist(
+      min, max); // distribution in range [min, max]
+  return dist(rng);
+}
+
+Vec3 get_random_Vec3(float min, float max){
+  return Vec3(get_random_float(min,max),get_random_float(min,max),get_random_float(min,max));
+}
+
+Vec3 random_in_unit_sphere() {
+// 随机在单位球上采样光线
+  while (true) {
+    auto p = get_random_Vec3(-1,1);
+    if (p.squared_length() >= 1) continue;
+    return p;  //这里先归一化了,后面多数使用这个版本
+  }
+}
+
+// 暂时新加，减少代码修改量，稍后修改
+Vec3 random_unit_vector() {
+// 归一化的采样
+    return unit_vector(random_in_unit_sphere());
+}
+
+Vec3 random_sample_hemisphere(const Vec3 &normal) {
+// 随机在单位半球上采样光线
+  Vec3 in_unit_sphere = random_in_unit_sphere();
+  if (dot(in_unit_sphere, normal) > 0.0) // In the same hemisphere as the normal
+    return in_unit_sphere;
+  else
+    return -in_unit_sphere;
+
+}
+Vec3 reflect(const Vec3 &v, Vec3 &n) {  
+// v:输入光线方向
+// n:物体表面法线
+// 返回反射光线的位置
+  return v - 2*dot(v,n)*n;
+}
+
+Vec3 refract(Vec3 &v, Vec3 &n, float ior) {
+// ior:物体的折射率
+// 返回一个折射后的光线
+  auto cos_theta = fmin(dot(-v, n), 1.0);
+  Vec3 r_out_perp =  ior * (v + cos_theta*n);
+  Vec3 r_out_parallel = -sqrt(fabs(1.0 - r_out_perp.squared_length())) * n;
+  return r_out_perp + r_out_parallel;
+}
+
+float schlick_reflectance(float cos, float ior) {
+  // Use Schlick's approximation for reflectance.
+  auto r0 = (1-ior) / (1+ior);
+  r0 = r0*r0;
+  return r0 + (1-r0)*pow((1 - cos),5);
+}
+
+Material::Material(Vec3 albedo_, MaterialType type_) {
+  albedo = albedo_;
+  type = type_;
+}
+
+Material::Material(Vec3 albedo_, float fuzz_, MaterialType type_) {
+  albedo = albedo_;
+  type = type_;
+  fuzz = std::min<float>(fuzz_, 1);
+}
+
+Material::Material(float ior_, MaterialType type_) {
+  ior = ior_;
+  type = type_;
+}
+
+Material::~Material() {}
+
+bool Material::scatter(const Ray &r, hit_record &rec, Ray &scattered,Vec3 &attenu) {
+// rec ：光线与物体相交的点的信息
+// scattered：光线与物体作用之后的反射方向
+// attenu ：能量衰减
+  switch (type) {
+  case Diffuse: {
+    auto scatter_direction = rec.normal + random_unit_vector();
+    // p->s: = rec.p + rec.normal + random_in_square
+    // 如果随机生成的向量在所有维度上都非常接近于零，会出错，拦截这样的可能
+    if (scatter_direction.near_zero())
+      scatter_direction = rec.normal;
+    scattered = Ray(rec.p, scatter_direction);
+    attenu = albedo;
+    return true;
+    break;
+  }
+
+  case Metal: {
+
+    Vec3 reflected = reflect(unit_vector(r.Direction()), rec.normal);
+    scattered = Ray(rec.p, reflected + fuzz*random_in_unit_sphere());
+    attenu = albedo;
+    return (dot(scattered.Direction(), rec.normal) > 0);
+
+    // return true;
+    break;
+  }
+
+  case Dielectric: {
+    rec.set_face_normal(r,rec.normal);
+    attenu = Vec3(1.0, 1.0, 1.0);
+    double refraction_ratio = rec.front_face ? (1.0/ior) : ior;
+    Vec3 unit_direction = unit_vector(r.Direction());
+    double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
+    double sin_theta = sqrt(1.0 - cos_theta*cos_theta);
+
+    bool cannot_refract = refraction_ratio * sin_theta > 1.0;   //全反射条件
+    Vec3 direction;
+    if (cannot_refract || schlick_reflectance(cos_theta, refraction_ratio) > get_random_float(0,1))
+      direction = reflect(unit_direction, rec.normal);
+    else
+      direction = refract(unit_direction, rec.normal, refraction_ratio);
+
+    scattered = Ray(rec.p, direction);
+
+    return true;
+    break;
+  }
+
+  default: {
+    std::cout << "Unsurportted Materail type!" << std::endl;
+    return false;
+    break;
+  }
+  }
+}
